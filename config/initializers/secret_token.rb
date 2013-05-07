14dd8df40713adbf96a6ceefd56b0e2914582608
John Ivnaoff <john.ivanoff@gmail.com>
2013-05-07 15:21:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobileBriggsLive::Application.config.secret_key_base = '253ab3b4c46ee080a32516599a2fdc9f0151383b6e56a47bdb1551ceec000f8663a3bc41b8ebc9a49034d2ac41e15e8f8e8bea1d88ac6fe39c916e660c6b07c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobileBriggsLive::Application.config.session_store :cookie_store, key: '_MobileBriggsLive_session'
